72ea3b8d74c24c8b9cdb0de3454b27a27d46873e
Kazilt <kazilfr@gmail.com>
2023-03-15 16:40:11 -0400

    Updated Gradle

--- app/src/main/java/com/example/layoutelements/MainActivity.kt
+++ app/src/main/java/com/example/layoutelements/MainActivity.kt
@@ -2,14 +2,29 @@ package com.example.layoutelements
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Switch
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.children
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toGame = findViewById<Button>(R.id.play)
+        val linearLayout = findViewById<LinearLayout>(R.id.ll)
+        val radioGroup = findViewById<RadioGroup>(R.id.rg)
+        toGame.setOnClickListener {
 
+        }
 
     }
 }
